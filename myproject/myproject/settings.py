ed02213e0a9ae76cd12414c08f59a75b474d917e
Julio Tabea <juliot4834@gmail.com>
2025-10-02 13:42:45 +0700

    Fersi akhir pengembangan JDIH untuk python 3.10.11 ke dua ( memperbaharui database ke deployment )

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -80,12 +80,18 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
         # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': config('DB_NAME'),
-        # 'USER': config('DB_USER'),
-        # 'PASSWORD': config('DB_PASSWORD'),
+        # 'NAME': 'jdih_unima',
+        # 'USER': 'root',
+        # 'PASSWORD': '',
         # 'HOST': '127.0.0.1',
         # 'PORT': '3306',
     }
